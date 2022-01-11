e25fa76bb85e2d38b64975cf70a10b3b0f587ee5
Masho Ado <mashoado@gmail.com>
2022-01-12 00:00:45 +0300

    open or close the menu on clicking the menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React, { useState } from "react";
 import "../Styles/menu-styles.css";
 
 function Header() {
+	// open/close menu
+	const [isOpen, setIsOpen] = useState(false);
+	const toggle = () => setIsOpen(!isOpen);
+
 	return (
 		<header className="header-section">
 			<div className="logo-menuBtn">
@@ -10,7 +14,10 @@ function Header() {
 					<h1 className="designo-logo">DESIGNO</h1>
 				</div>
 
-				<div className="menu-btn">
+				<div
+					className={isOpen ? "menu-btn close-btn" : "menu-btn"}
+					onClick={toggle}
+				>
 					<div className="menu-bar"></div>
 					<div className="menu-bar"></div>
 					<div className="menu-bar"></div>
