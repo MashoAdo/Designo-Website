715bd6b883bb955c406eb3d08b8bc94b5b460aa9
Masho Ado <mashoado@gmail.com>
2022-01-14 22:10:19 +0300

    footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CallToActionComponent from "./CallToActionComponent";
+import FooterInformation from "./FooterInformation";
+
+function Footer() {
+	return (
+		<footer>
+			<CallToActionComponent />
+			<FooterInformation />
+		</footer>
+	);
+}
+
+export default Footer;
