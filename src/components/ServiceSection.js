682746b7438f51c5ef4e1ee091a8f4822e08b549
Masho Ado <mashoado@gmail.com>
2022-01-14 22:28:40 +0300

    import jps urls and send to service item component as props ,to be used as background url

--- src/components/ServiceSection.js
+++ src/components/ServiceSection.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ServiceItem from "./ServiceItem";
+import webDesign from "../assets/home/mobile/image-web-design.jpg";
+import appDesign from "../assets/home/mobile/image-app-design.jpg";
+import graphicDesign from "../assets/home/mobile/image-graphic-design.jpg";
 
 function Services() {
 	return (
 		<div className="services-section">
-			<ServiceItem name="Web Design" />
-			<ServiceItem name="App Design" />
-			<ServiceItem name="Graphic Design" />
+			<ServiceItem backgroundImageUrl={webDesign} name="Web Design" />
+			<ServiceItem backgroundImageUrl={appDesign} name="App Design" />
+			<ServiceItem backgroundImageUrl={graphicDesign} name="Graphic Design" />
 		</div>
 	);
 }
