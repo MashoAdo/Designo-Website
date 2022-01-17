7a799fe0beab9636b3d3d2ce2748918afea5fd52
Masho Ado <mashoado@gmail.com>
2022-01-18 00:24:24 +0300

    add div with class name hero-container to make it to  position using display

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,17 +6,19 @@ import { ReactComponent as Phone } from "../assets/phone.svg";
 function Hero() {
 	return (
 		<div className="hero-section">
-			<div className="hero-content">
-				<h2 className="hero-header">
-					Award-winning custom designs and digital branding solutions
-				</h2>
-				<p>
-					With over 10 years in the industry, we are experienced in creating
-					fully responsive websites, app design, and engaging brand experiences.
-					Find out more about our services.
-				</p>
+			<div className="hero-container">
+				<div className="hero-content">
+					<h2 className="hero-header">
+						Award-winning custom designs and digital branding solutions
+					</h2>
+					<p>
+						With over 10 years in the industry, we are experienced in creating
+						fully responsive websites, app design, and engaging brand
+						experiences. Find out more about our services.
+					</p>
+				</div>
+				<button className="light-button">LEARN MORE</button>
 			</div>
-			<button className="light-button">LEARN MORE</button>
 			<Phone />
 		</div>
 	);
