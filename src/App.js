2c5c1632f7319d628fadf9dab55c1f70e208bece
Masho Ado <mashoado@gmail.com>
2022-01-18 00:16:06 +0300

    add components in the body and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
 import React from "react";
+import "./App.css";
 import Header from "./components/Header.js";
+import Hero from "./components/Hero";
+import Qualities from "./components/Qualities.js";
+import Services from "./components/ServiceSection.js";
+import Footer from "./components/Footer.js";
 
 function App() {
 	return (
-		<div className="App">
+		<div className="app">
 			<Header />
+			<body>
+				<Hero />
+				<Services />
+				<Qualities />
+			</body>
+			<Footer />
 		</div>
 	);
 }
