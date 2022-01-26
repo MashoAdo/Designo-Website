b88b266b82d6b8396244765fd397ba966f49d501
Masho Ado <mashoado@gmail.com>
2022-01-26 23:29:59 +0300

    add path to locations page

--- src/AboutPage/AboutLocationItem.js
+++ src/AboutPage/AboutLocationItem.js
@@ -2,18 +2,21 @@ import React from "react";
 
 // styles
 import "../AboutStyles/AboutLocation.css";
+import { Link } from "react-router-dom";
 
 function AboutLocationItem({ name, svgComponent, margin }) {
 	return (
-		<div className="about-location-item" style={{ marginBottom: margin }}>
-			<div className="location-image-wrapper">{svgComponent}</div>
+		<Link to="/locations">
+			<div className="about-location-item" style={{ marginBottom: margin }}>
+				<div className="location-image-wrapper">{svgComponent}</div>
 
-			<div className="location-info">
-				<span>{name}</span>
+				<div className="location-info">
+					<span>{name}</span>
 
-				<button>SEE LOCATION</button>
+					<button>SEE LOCATION</button>
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 }
 
